d83700470a3d9a9bca9ca60935e4e89c2299495c
Jorgeflowers18 <jefo20156@gmail.com>
2019-04-11 11:10:41 -0500

    Clase 2.1

---
+++ EjemplosClase2/src/ejemplosclase2/EjemplosClase2_2.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplosclase2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jorgeso
+ */
+public class EjemplosClase2_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nombre;
+        String apellido;
+        int edad;
+        String ciudad;
+        Scanner tec = new Scanner(System.in);
+
+        System.out.println("Por favor ingrese sus nombre:");
+        nombre = tec.nextLine();
+
+        System.out.println("Por favor ingrese sus apellido:");
+        apellido = tec.nextLine();
+
+        System.out.println("Por favor ingrese su edad:");
+        edad = tec.nextInt();
+        
+        tec.nextLine();//limpieza de buffer de entrada de datos
+        
+        System.out.println("Por favor ingrese su ciudad:");
+        ciudad = tec.next();
+        
+        System.out.println("Su nombre es: " + nombre + "\n\n\tSu apellido es: "
+                + apellido + ", edad: " + edad + "\nCiudad: " + ciudad);
+    
+                
+    }
+
+}
